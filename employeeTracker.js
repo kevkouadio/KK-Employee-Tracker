be57eab53b6226e008c4bb4321865daaeaa565bc
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-09-18 07:50:34 -0400

    set of viewEmployeesByDept function

--- employeeTracker.js
+++ employeeTracker.js
@@ -25,7 +25,7 @@ function startApp() {
   inquirer
     .prompt({
       name: "action",
-      type: "rawlist",
+      type: "list",
       message: "What would you like to do?",
       choices: [
         "View All Employees",
@@ -78,5 +78,34 @@ function viewEmployees() {
         console.table(query);
         startApp();
     });
+};
 
-}
+function viewEmployeesByDept() {
+    var query =`SELECT departments.name AS department, employees.id, employees.first_name, employees.last_name, role.title FROM employees LEFT JOIN role on 
+    employees.role_id = role.id LEFT JOIN departments departments on role.department_id = departments.id WHERE departments.id;`;
+    connection.query(query, function(err, query){
+      console.table(query);
+      startApp();
+});
+};
+
+function viewEmployeesByMng() {
+
+};
+
+function addEmployee() {
+
+};
+
+function removeEmployee() {
+
+};
+
+function updateEmployeeRole() {
+
+};
+
+function updateEmployeeMng() {
+
+};
+
